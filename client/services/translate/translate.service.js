f4ea9dc88aedd314dba1ad485197621f3c47fabf
gqueste <gabriel.queste@gmail.com>
2015-02-05 17:00:22 +0100

    **feat** : feature de registres

--- client/services/translate/translate.service.js
+++ client/services/translate/translate.service.js
@@ -3,12 +3,24 @@
 
     var english_translation = {
         'MESSAGE' : 'Hello world',
-        'REGISTRIES_TITLE' : 'Registries'
+        'REGISTRIES_TITLE' : 'Registries',
+        'REGISTRIES_ID' : 'Id',
+        'REGISTRIES_NAME' : 'Name',
+        'REGISTRIES_TYPE' : 'Type',
+        'REGISTRIES_HOST' : 'Host',
+        'REGISTRIES_PORT' : 'Port',
+        'REGISTRIES_PROTOCOLE' : 'Protocole'
     };
 
     var french_translation = {
         'MESSAGE' : 'Bonjour le monde',
-        'REGISTRIES_TITLE' : 'Registres'
+        'REGISTRIES_TITLE' : 'Registres',
+        'REGISTRIES_ID' : 'Id',
+        'REGISTRIES_NAME' : 'Nom',
+        'REGISTRIES_TYPE' : 'Type',
+        'REGISTRIES_HOST' : 'Hôte',
+        'REGISTRIES_PORT' : 'Port',
+        'REGISTRIES_PROTOCOLE' : 'Protocole'
     };
 
     angular
